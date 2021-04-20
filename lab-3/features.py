cc9ef27a97409933522d151c427a3e5d6feace91
Bashir <32179747+FUUT0N@users.noreply.github.com>
2021-04-20 23:00:11 +0300

    Add files via upload

---
+++ lab-3/features.py
@@ -0,0 +1,25 @@
+import cv2
+import mahotas
+bins = 8
+
+
+# Hu Moments
+def fd_hu_moments(image):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    feature = cv2.HuMoments(cv2.moments(image)).flatten()
+    return feature
+
+
+# Haralick Texture
+def fd_haralick(image):
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    haralick = mahotas.features.haralick(gray).mean(axis=0)
+    return haralick
+
+
+# Color Histogram
+def fd_histogram(image, mask=None):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+    hist = cv2.calcHist([image], [0, 1, 2], None, [bins, bins, bins], [0, 256, 0, 256, 0, 256])
+    cv2.normalize(hist, hist)
+    return hist.flatten()
